384ad05ef5fb57cec4e810db5d8710f928cd39d9
Vlad Zhura <85967102+ZhuraTown@users.noreply.github.com>
2022-07-03 21:21:53 +0300

    add edit profile, add logging

--- config.py
+++ config.py
@@ -9,3 +9,10 @@ class Config:
     SECRET_KEY = config("SECRET_KEY_FLASK") or 'secret-key'
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(base_dir, 'app.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = config('MAIL_SERVER')
+    MAIL_PORT = int(config('MAIL_PORT') or 25)
+    MAIL_USE_TLS = config('MAIL_USE_TLS', default=None) is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']

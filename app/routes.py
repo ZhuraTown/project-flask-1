372d783825a58baf7c7b001be04548d2bc1d99bb
Vlad Zhura <85967102+ZhuraTown@users.noreply.github.com>
2022-07-03 15:09:44 +0300

    add register user

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,36 @@
 # -*- coding: utf-8 -*-
 
 from flask import render_template, redirect, url_for, request, flash
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
 
 
 @app.route('/', methods=['GET'])
+@login_required
 def main():
-    return render_template('main.html')
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
+        }
+    ]
+    return render_template('main.html', posts=posts)
 
 
 @app.route('/blog', methods=['GET'])
+@login_required
 def blog():
     return render_template('blog.html')
 
@@ -21,24 +40,40 @@ def about():
     return render_template('about.html')
 
 
-# @app.route('/auth', methods=['GET'])
-# def auth():
-#     return render_template('auth.html')
-
-
 @app.route('/register_user', methods=["GET", "POST"])
 def register_user():
-    if request.method == "POST":
-        print('lala')
-        return render_template('register_form.html')
-    elif request.method == "GET":
-        return render_template('register_form.html')
+    if current_user.is_authenticated:
+        return redirect(url_for('main'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Регистрация, прошла успешно')
+        return redirect(url_for('main'))
+    return render_template('register_form.html', title='Регистрация', form=form)
 
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
-        return redirect(url_for('main'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('main')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main'))
